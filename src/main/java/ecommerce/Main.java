cce8208a0dd1d3e8631e0dd87061f81ad2a7f14c
varaprasadGit <109330149+varaprasadGit@users.noreply.github.com>
2024-05-22 16:33:55 +0530

    my 1st commit

---
+++ src/main/java/ecommerce/Main.java
@@ -0,0 +1,17 @@
+package ecommerce;
+
+public class Main {
+    public static void main(String[] args) {
+        String name="vara123@!@#$%^&*()_+prasad456";
+
+        //regular experession -- REGEX
+        String alph= name.replaceAll("[^a-zA-Z]","");
+        String num= name.replaceAll("[^0-9]","");
+        String SplCh = name.replaceAll("[a-zA-Z,0-9]","");
+        System.out.println("Alphabets: " +  alph);
+        System.out.println("Numbers: " +  num);
+        System.out.println("Special Charactors: " +  SplCh);
+
+
+    }
+}
